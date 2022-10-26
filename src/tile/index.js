785a207cf088bfa52b1e8f4a40cec1aa8f0fcd89
Merey <merey.90@gmail.com>
2022-10-26 20:06:35 +0200

    animate when found

--- src/tile/index.js
+++ src/tile/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 import { PlayerWrapper, StyledPlayer, TileWrapper } from './styles';
 import * as question from './question.json';
 
@@ -6,13 +8,26 @@ export const Tile = ({ asset, state, id, handleTileClick }) => {
   if (state.isOpen) mainClass += 'is-flipped';
   if (state.isFound) mainClass += 'is-found';
 
+  const [controls, setControls] = useState();
+
+  useEffect(() => {
+    controls?.setSeeker(controls?.state?.instance?.totalFrames / 3);
+    if (state.isFound) controls?.play();
+    if (!state.isFound) controls?.pause();
+  }, [state.isFound, controls, controls?.setSeeker]);
+
   return (
     <TileWrapper onClick={() => handleTileClick(id)} className={mainClass}>
       <PlayerWrapper className={'question'}>
         <StyledPlayer autoplay loop src={question} />
       </PlayerWrapper>
       <PlayerWrapper className={'answer'}>
-        <StyledPlayer autoplay loop src={asset} />
+        <StyledPlayer
+          ref={(instance) => setControls(instance)}
+          autoplay
+          loop
+          src={asset}
+        />
       </PlayerWrapper>
     </TileWrapper>
   );
